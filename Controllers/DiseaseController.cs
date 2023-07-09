07c27f43d9eee7345444e511b18e384b920b2765
Nayeem45s <nayeem.mu01936@gmail.com>
2023-07-09 21:54:27 +0600

    New Project Add

---
+++ Controllers/DiseaseController.cs
@@ -0,0 +1,16 @@
+﻿using ExcellProgramTest.Data;
+using ExcellProgramTest.Entites;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ExcellProgramTest.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DiseaseController : BaseController<DiseaseInformation>
+    {
+        public DiseaseController(IRepository<DiseaseInformation> repository) : base(repository)
+        {
+            
+        }
+    }
+}
